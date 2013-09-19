d331835b2907042d39e2d09d2c18b2df650e6d2c
Harrison Brown <harrisonbro@gmail.com>
2013-09-19 18:01:31 +0100

    Added prefetching examples

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -15,6 +15,11 @@
 		<!-- Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page -->
   		<!-- See: http://www.buyog.com/code/?id=53 -->
   		<meta http-equiv="x-dns-prefetch-control" content="off">
+  		
+  		<!-- Pre-fetch domains of assets requested later in the document -->
+  		<!-- <link rel="dns-prefetch" href="//example.tld"> -->
+		<!-- Pre-fetch assets globally-used assets (but *not* JavaScript) -->
+		<!-- <link rel="prefetch" href="asset.ext"> -->
   
 		<?php wp_head(); ?>
 	</head>
