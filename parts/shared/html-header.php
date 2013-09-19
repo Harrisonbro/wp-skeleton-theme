e321b4d058e0387aaa283e89bc31b4fc84b126d4
Harrison Brown <harrisonbro@gmail.com>
2013-09-19 18:00:13 +0100

    Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -11,6 +11,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		
+		<!-- Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page -->
+  		<!-- See: http://www.buyog.com/code/?id=53 -->
+  		<meta http-equiv="x-dns-prefetch-control" content="off">
+  
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
