9e03e05a22301608f5e5abfb8a092c7ac23df9ce
Harrison Brown <harrisonbro@gmail.com>
2013-09-26 17:39:52 +0100

    Added wp-skeleton /wp/ path to THEME_ROOT definition

--- functions/definitions.php
+++ functions/definitions.php
@@ -1,6 +1,6 @@
 <?php 
  
-defined('THEME_PATH')   ? null : define('THEME_PATH', SITE_ROOT."/wp-content/themes/wp-skeleton-theme");
+defined('THEME_PATH')   ? null : define('THEME_PATH', SITE_ROOT."/wp/wp-content/themes/wp-skeleton-theme");
 defined('LIB_PATH')     ? null : define('LIB_PATH', THEME_PATH."/functions/lib");
 defined('HOME_URI')     ? null : define('HOME_URI', get_bloginfo('url'));
 defined('THEME_URI')    ? null : define('THEME_URI', get_stylesheet_directory_uri());
