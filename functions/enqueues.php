0081af574dc0e008c3921caf6578f214ac9bef89
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2013-09-27 08:12:48 +0100

    Commenting and formatting

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -2,13 +2,20 @@
 
 function AblosMedia_enqueuer() {
 
-	/* wp_register_script( $handle, $src, $deps, $ver, $in_footer ) */
+	/**
+	 * Theme JavaScript
+	 * wp_register_script( $handle, $src, $deps, $ver, $in_footer )
+	 */
 	// wp_register_script( 'script', THEME_JS.'/script.js', array( 'jquery' ) );
 	// wp_enqueue_script( 'script' );
 
+	/**
+	 * Theme CSS
+	 * wp_register_style( $handle, $src, $deps, $ver, $media )
+	 */
+
 	global $wp_styles; 
 
-	/* wp_register_style( $handle, $src, $deps, $ver, $media ) */
 	wp_register_style( 'all', THEME_CSS.'/all.css', '', '', 'screen' );
   wp_enqueue_style( 'all' );
 
