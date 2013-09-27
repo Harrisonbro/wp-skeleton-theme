e6a7784db371576c6a79834d35702bf8277af854
Harrison Brown (MacBook Pro) <harrisonbro@gmail.com>
2013-09-27 08:11:14 +0100

    Added conditional loading to let IE8 styles

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -6,8 +6,14 @@ function AblosMedia_enqueuer() {
 	// wp_register_script( 'script', THEME_JS.'/script.js', array( 'jquery' ) );
 	// wp_enqueue_script( 'script' );
 
+	global $wp_styles; 
+
 	/* wp_register_style( $handle, $src, $deps, $ver, $media ) */
-	// wp_register_style( 'stylesheet', THEME_CSS.'/stylesheet.css', '', '', 'screen' );
-  //  wp_enqueue_style( 'stylesheet' );
+	wp_register_style( 'all', THEME_CSS.'/all.css', '', '', 'screen' );
+  wp_enqueue_style( 'all' );
+
+  wp_register_style( 'all-lte-ie8', THEME_CSS.'/all-lte-ie8.css', '', '', 'screen' );
+  wp_enqueue_style( 'all-lte-ie8' );
+  $wp_styles->add_data( 'all-lte-ie8', 'conditional', 'lte IE 8'); 
 
 }	
