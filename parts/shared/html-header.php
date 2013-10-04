6976a9b7aff0b7ec68b7c4e172c4ef38c544d33d
Harrison Brown <harrisonbro@gmail.com>
2013-10-04 09:30:46 +0100

    Added favicon, MS Windows tile icons & colour, and apple touch icons

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -12,7 +12,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
   
-  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+  <!-- Apple touch icons. See http://mathiasbynens.be/notes/touch-icons -->
+  <link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/img/apple-touch-icon-152x152-precomposed.png">
+  <!-- Favicons. See http://www.jonathantneal.com/blog/understand-the-favicon/ -->
+  <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.png">
+  <!--[if IE]>
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+  <![endif]-->
+  <meta name="msapplication-TileColor" content="#D83434">
+  <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/img/tileicon.png">
   
   <!-- Prevent some browsers (eg. Chrome) attempting to pre-fetch all URLs on your page -->
   <!-- See: http://www.buyog.com/code/?id=53 -->
