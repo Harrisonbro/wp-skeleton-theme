56a3456b7002eca5f0b7092a1a39457e46958d67
Harrison Brown <harrisonbro@gmail.com>
2013-10-07 13:00:19 +0100

    Hide WordPress Update Notice To All But Admins

--- functions/admin.php
+++ functions/admin.php
@@ -6,3 +6,13 @@ add_editor_style( THEME_CSS.'/wp-admin-editor.css' );
 /* Metaboxes */
 // include_once 'metaboxes/setup.php';
 // include_once 'metaboxes/...-spec.php';
+
+/**
+ * Hide WordPress Update Notice To All But Admins
+ */
+function hide_update_notice_to_all_but_admin_users() {
+	if (!current_user_can('update_core')) {
+		remove_action( 'admin_notices', 'update_nag', 3 );
+	}
+}
+add_action( 'admin_notices', 'hide_update_notice_to_all_but_admin_users', 1 );
